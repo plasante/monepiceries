1efc94ff3ac2757cb0191ef27cb6669cbcaab9ae
Pierre Lasante <pierre.lasante@videotron.ca>
2017-04-08 08:42:23 -0400

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,28 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+  
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user, options = {})
+    password = options[:password]  || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+    else 
+      session[:user_id] = user.id
+    end
+  end
+  
+  private
+    
+    def integration_test?
+      true
+    end
 end
