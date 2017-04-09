72860d42c41c9d629e801f196dcd76ee71a67b9a
Pierre Lasante <pierre.lasante@videotron.ca>
2017-04-09 09:01:03 -0400

    Finis user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -9,8 +9,11 @@ ruby '2.4.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
-gem 'bootstrap-sass', '3.3.6'
-gem 'bcrypt',         '3.1.11'
+gem 'bootstrap-sass',          '3.3.6'
+gem 'bcrypt',                  '3.1.11'
+gem 'faker',                   '1.6.6'
+gem 'will_paginate',           '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Pierre Lasante",
+             email: "pierre.lasante@videotron.ca",
+             password:              "123456",
+             password_confirmation: "123456",
+             admin: true)
+
+User.create!(name:  "Carole Spenard",
+             email: "cspenard@videotron.ca",
+             password:              "123456",
+             password_confirmation: "123456")
+             
+User.create!(name:  "Juby Spenard",
+             email: "jspenard@videotron.ca",
+             password:              "123456",
+             password_confirmation: "123456")
+
+97.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
